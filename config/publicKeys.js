671b46a291e2d72c10eed757a86ed48f92ee7978
Nicholas Iyamabo <niyamabo@gmail.com>
2022-10-26 19:37:40 +0100

    production database

--- config/publicKeys.js
+++ config/publicKeys.js
@@ -1,4 +1,4 @@
 module.exports = {
     mongoURI:
-    `mongodb+srv://spreadtheworddev:${process.env.MONGODB_KEY}@cluster0.ole5n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    `${process.env.MONGODB_URI}`
 }
